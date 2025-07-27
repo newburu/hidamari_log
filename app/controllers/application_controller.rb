de951acf8acd209aabc04c54e09954ccac1f373b
Newburu <newburu@gmail.com>
2025-07-27 13:46:36 +0900

    カレンダーのスタイルをカスタマイズ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,15 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
 
   helper_method :current_user
+  before_action :set_beginning_of_week
 
   private
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def set_beginning_of_week
+    Date.beginning_of_week = :sunday
   end
 end
