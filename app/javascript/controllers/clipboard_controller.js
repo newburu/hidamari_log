e1727d07d39dd7aabdda47945c8e6ecbb939e908
Newburu <newburu@gmail.com>
2025-08-13 21:05:59 +0900

    コピペを楽にする #9

---
+++ app/javascript/controllers/clipboard_controller.js
@@ -0,0 +1,11 @@
+// app/javascript/controllers/clipboard_controller.js
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ "source" ]
+
+  copy() {
+    navigator.clipboard.writeText(this.sourceTarget.textContent)
+    alert("コピーしました！")
+  }
+}
